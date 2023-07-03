c3a4d5ede821336ceac5a241afe85db683122a85
Vijayalakshmi <vijayalakshmilanka997@gmail.com>
2023-07-03 08:51:33 +0000

    Super Over League Tets Solution Using React JS

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="image-alignment">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="images"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="images"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
